142faab2bf18900f96a020e4ab0741cb825c61f7
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-01-02 22:45:55 +0800

    add state machine

--- delight.d
+++ delight.d
@@ -20,20 +20,8 @@ void main( string args[] )
 	auto w = File( filename ~ "d", "w" );
 
 	// Get tokens from lexer
-	lexer l = new lexer( args[1] );
-	parser p = new parser();
-	string token, result;
+	parser p = new parser( args[1] );
+	string result = p.parse();
 
-	// While we still have tokens
-	while ( !l.is_empty() )
-	{
-		// Get a token from the lexer
-		token = l.pop();
-
-		// Process the token
-		result = p.process( token );
-
-		// write out source
-		w.write( result );
-	}
+	w.write( result );
 }
--- parser.d
+++ parser.d
@@ -3,9 +3,13 @@
  * - checks for syntax errors
  * - generates valid d code
  */
+import lexer;
+import std.algorithm;
+import std.conv;
+
 class parser
 {
-	string state;
+	lexer l;
 
 	immutable string[] assignment_operators = [
 		"=",
@@ -45,25 +49,6 @@ class parser
 		"or"
 	];
 
-	immutable string[] statements = [
-		"assert",
-		"break",
-		"catch",
-		"continue",
-		"do",
-		"finally",
-		"foreach",
-		"foreach_reverse",
-		"import",
-		"mixin",
-		"return",
-		"switch",
-		"throw",
-		"try",
-		"typeof"
-		"while"
-	];
-
 	immutable string[] operators = [
 		"+",
 		"-",
@@ -87,6 +72,25 @@ class parser
 		"#"
 	];
 
+	immutable string[] statements = [
+		"assert",
+		"break",
+		"catch",
+		"continue",
+		"do",
+		"finally",
+		"foreach",
+		"foreach_reverse",
+		"import",
+		"mixin",
+		"return",
+		"switch",
+		"throw",
+		"try",
+		"typeof"
+		"while"
+	];
+
 	immutable string[] types = [
 		"auto", "bool",
 		"byte", "short", "int", "long", "cent",
@@ -97,7 +101,7 @@ class parser
 		"char", "wchar", "dchar"
 	];
 
-	immutable string[] user_type = [
+	immutable string[] user_types = [
 		"alias",
 		"class",
 		"enum",
@@ -105,13 +109,236 @@ class parser
 		"union"
 	];
 
-	this()
+	this( string filename )
 	{
-		state = "start state";
+		l = new lexer( filename );
 	}
 
-	string process( string token )
+	string identify_symbol( string token )
 	{
-		return token;
+		if ( find( [ " ", "\n" ], token ) )
+			return token;
+		else if ( find( assignment_operators, token ) )
+			return "assignment operator";
+		else if ( find( attributes, token ) )
+			return "attribute";
+		else if ( find( logical, token ) )
+			return "logical";
+		else if ( find( operators, token ) )
+			return "operator";
+		else if ( find( punctuation, token ) )
+			return "punctuation";
+		else if ( find( statements, token ) )
+			return "statement";
+		else if ( find( types, token ) )
+			return "type";
+		else if ( find( user_types, token ) )
+			return "user type";
+		else
+			return "identifier";
+	}
+	
+	string parse()
+	{
+		return start_state( l.pop() );
+	}
+
+	string start_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "statement":
+				if ( token == "import" )
+					return token ~ im_state( l.pop() );
+				else
+					throw unexpected( token );
+			case "type":
+				return token ~ declare_state( l.pop() );
+			case "\n":
+				return token ~ start_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string im_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return library_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	};
+
+	string library_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "punctuation":
+				if ( token != "." )
+					throw unexpected( token );
+				else
+					return token ~ period_state( l.pop() );
+			case "\n":
+				return ";\n" ~ start_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string period_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return token ~ library_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string declare_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return token ~ declared_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string declared_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "punctuation":
+				if ( token == "(" )
+					return token ~ function_declaration_state( l.pop() );
+				else if ( token == "," )
+					return token ~ declare_state( l.pop() );
+				else
+					throw unexpected( token );
+			case "\n":
+				return ";\n" ~ start_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string function_declaration_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "type":
+				return token ~ function_variable_state( l.pop() );
+			case "punctuation":
+				if ( token == ")" )
+					return token ~ colon_state( l.pop() );
+				else
+					throw unexpected( token );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string function_variable_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return token ~ function_declaration_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string colon_state( string token )
+	{
+		switch ( token )
+		{
+			case "punctuation":
+				if ( token == ":" )
+					return " {" ~ ready_state( l.pop() );
+				else
+					throw unexpected( token );
+			default:
+				throw new Exception( "On line " ~ to!string( l.line_number ) ~ " expected ':'" );
+		}
+	}
+
+	string ready_state( string token )
+	{
+		if ( l.is_empty() )
+			return "";
+		switch ( identify_symbol( token ) )
+		{
+			case "\n":
+				return token ~ ready_state( l.pop() );
+			case "type":
+				return token ~ declare_state( l.pop() );
+			case "identifier":
+				return token ~ identifier_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string identifier_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "punctuation":
+				if ( token == "(" )
+					return token ~ function_call_state( l.pop() );
+				else
+					throw unexpected( token );
+			case "assignment_operator":
+				return token ~ assignment_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string function_call_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return token ~ function_call_state( l.pop() );
+			case "punctuation":
+				if ( token == ")" )
+					return token ~ endline_state( l.pop() );
+				else
+					throw unexpected( token );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string assignment_state( string token )
+	{
+		switch ( identify_symbol( token ) )
+		{
+			case "identifier":
+				return token ~ endline_state( l.pop() );
+			default:
+				throw unexpected( token );
+		}
+	}
+
+	string endline_state( string token )
+	{
+		if ( token == "\n" )
+			return ";\n" ~ ready_state( l.pop() );
+		else
+			throw unexpected( token );
+	}
+
+	Exception unexpected( string token )
+	{
+		return new Exception( "On line " ~ to!string( l.line_number ) ~ ": unexpected token '" ~ token ~ "'" );
 	}
 }
