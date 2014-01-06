a8c7e51cfb2c9969b591a1e71111c887507fe399
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-01-06 16:18:31 +0800

    Clean up file with "main" and add some documentation

--- delight.d
+++ delight.d
@@ -1,27 +1,42 @@
+/**
+ * Main for Delight programming language preprocessor.
+ * Author: Peter Plantinga
+ *
+ * This program can be called from the command line using:
+ * ----------------
+ * delight {filename}.delight
+ * ----------------
+ *
+ * This will parse the file and create another file '{filename}.d'
+ * which is valid code in the d programming language. Run
+ * ----------------
+ * dmd {filename}.d
+ * ----------------
+ * to create an executable.
+ */
 import std.stdio;
 import std.algorithm;
-import std.range;
 import std.conv;
 import lexer;
 import parser;
 
 void main( string args[] )
 {
-	// Check for arguments
-	if ( args.length < 2 || find( args[1], "." ) != ".delight" )
+	// Check for appropriate arguments
+	if ( args.length < 2 || endsWith( args[1], ".delight" ) )
 	{
 		writeln( "Usage: delight [file.delight]" );
 		return;
 	}
 
-	// Extract filename
-	string reverse = to!(string)(retro( args[1] ));
-	string filename = to!(string)(retro( find( reverse, '.' ) ));
-	auto w = File( filename ~ "d", "w" );
-
-	// Get tokens from lexer
+	/// Parse the input file
 	parser p = new parser( args[1] );
 	string result = p.parse();
+	
+	/// Extract output filename by removing last 6 chars: test.d*elight*
+	string filename = args[1][0 .. $-6];
+	auto w = File( filename ~ "d", "w" );
 
+	// Write the results to the new file
 	w.write( result );
 }
