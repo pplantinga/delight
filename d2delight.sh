2e926019cb7b11f716e8171148cf223b5a7ba456
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-02-18 18:30:34 +0800

    split massive one-liner into reasonable parts

--- d2delight.sh
+++ d2delight.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 end="elight"
-cat $1 | sed -r 's/(import.*);/\1/g' | sed 's/void main()/procedure main:/' | sed -r 's/^(\s*)for\s*\(\s*(.*);\s*(.*);\s*(.*[^ ])\s*\)/\1\2\n\1while \3:\n\1\t\4/' | sed -r 's/foreach\s*\(\s*(.*);\s*(.*[^ ])\s*\)/for \1 in \2:/' | sed 's/++/ += 1/g' | sed 's/--/ -= 1/g' | sed -r 's/(else )?if\s*\(\s*(.*[^ ])\s*\)/\1if \2:/g' | sed 's/&&/and/g' | sed 's/||/or/g' | sed 's/</less than/g' | sed 's/>/more than/g' | sed 's/==/equal to/' | sed 's/<=/not more than/g' | sed 's/>=/not less than/g' | sed 's/[{}]//g' | sed 's/;$//' > "$1$end"
+sed -r -e 's/(import.*);/\1/g' \
+	-e 's/void main\(\)/procedure main:/' \
+	-e 's/^(\s*)for\s*\(\s*(.*);\s*(.*);\s*(.*[^ ])\s*\)/\1\2\n\1while \3:\n\1\t\4/' \
+	-e 's/foreach\s*\(\s*(.*);\s*(.*[^ ])\s*\)/for \1 in \2:/' \
+	-e 's/\+\+/ \+= 1/g' \
+	-e 's/--/ -= 1/g' \
+	-e 's/(else )?if\s*\(\s*(.*[^ ])\s*\)/\1if \2:/g' \
+	-e 's/&&/and/g' \
+	-e 's/\|\|/or/g' \
+	-e 's/</less than/g' \
+	-e 's/>/more than/g' \
+	-e 's/==/equal to/' \
+	-e 's/<=/not more than/g' \
+	-e 's/>=/not less than/g' \
+	-e 's/[{}]//g' \
+	-e 's/;$//' < $1 > "$1$end"
