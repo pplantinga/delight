89b015a2d2422b9870b07f366bfdad846bb8d782
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-02-21 23:16:17 +0800

    rearrange lines to do longer matches first

--- d2delight.sh
+++ d2delight.sh
@@ -10,10 +10,10 @@ sed -r -e 's/(import.*);/\1/g' \
 	-e 's/(else )?if\s*\(\s*(.*[^ ])\s*\)/\1if \2:/g' \
 	-e 's/&&/and/g' \
 	-e 's/\|\|/or/g' \
+	-e 's/<=/not more than/g' \
+	-e 's/>=/not less than/g' \
 	-e 's/</less than/g' \
 	-e 's/>/more than/g' \
 	-e 's/==/equal to/' \
-	-e 's/<=/not more than/g' \
-	-e 's/>=/not less than/g' \
 	-e 's/[{}]//g' \
 	-e 's/;$//' < $1 > "$1$end"
