953b8c31af75bcd2f95056f60e1817ad43f22e77
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-02-22 11:08:04 +0800

    add a few more conversions in d2delight script

--- d2delight.sh
+++ d2delight.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 end="elight"
-sed -r -e 's/(import.*);/\1/g' \
-	-e 's/void main\(\)/procedure main:/' \
-	-e 's/^(\s*)for\s*\(\s*(.*);\s*(.*);\s*(.*[^ ])\s*\)/\1\2\n\1while \3:\n\1\t\4/' \
-	-e 's/foreach\s*\(\s*(.*);\s*(.*[^ ])\s*\)/for \1 in \2:/' \
-	-e 's/\+\+/ \+= 1/g' \
+sed -e 's/\(import.*\);/\1/g' \
+	-e 's/void \([a-zA-Z0-9_]*(.*)\)/procedure \1:/' \
+	-e 's/^\(\s*\)for\s*(\s*\(.*\);\s*\(.*\);\s*\(.*[^ ]\)\s*)/\1\2\n\1while \3:\n\1\t\4/' \
+	-e 's/foreach\s*(\s*\(.*\);\s*\(.*[^ ]\)\s*)/for \1 in \2:/' \
+	-e 's/\(while\|switch\)\s*(\s*\(.*[^ ]\)\s*)/\1 \2:/' \
+	-e 's_/\*\*_#._' \
+	-e 's_/++_#._' \
+	-e 's_///_#._' \
+	-e 's_/\*_#_' \
+	-e 's_/+_#_' \
+	-e 's_//_#_' \
+	-e 's/++/ \+= 1/g' \
 	-e 's/--/ -= 1/g' \
-	-e 's/(else )?if\s*\(\s*(.*[^ ])\s*\)/\1if \2:/g' \
+	-e 's/\(else \)\?if\s*(\s*\(.*[^ ]\)\s*)/\1if \2:/g' \
 	-e 's/&&/and/g' \
-	-e 's/\|\|/or/g' \
+	-e 's/||/or/g' \
 	-e 's/<=/not more than/g' \
 	-e 's/>=/not less than/g' \
 	-e 's/</less than/g' \
 	-e 's/>/more than/g' \
-	-e 's/==/equal to/' \
+	-e 's/==/equal to/g' \
+	-e 's/!=/not equal to/g' \
 	-e 's/[{}]//g' \
 	-e 's/;$//' < $1 > "$1$end"
