57c2abce18a6f246c5c1d3cf61199d876476a8c5
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-02-23 17:39:41 +0800

    convert d2delight.sh to d2delight.py because sed does not support lookahead

---
+++ d2delight.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+if len( sys.argv ) != 2:
+	print "usage: d2delight.py path/to/file.d"
+
+with open( sys.argv[1], 'r' ) as file:
+	content = file.read()
+
+content = re.sub( r'(import.*);', r'\1', content )
+content = re.sub( r'\b(?!void)([a-z]+) ([a-zA-Z0-9_]+\(.*)\)', r'function \2-> \1 ):', content )
+content = re.sub( r'void ([a-zA-Z0-9_]*\(.*\))', r'procedure \1:', content )
+content = re.sub( r'\n([ \t]*)for\s*\(\s*(.*);\s*(.*);\s*(.*[^ ])\s*\)', r'\n\1\2\n\1while \3:\n\1\t\4', content )
+content = re.sub( r'foreach\s*\(\s*(.*);\s*(.*[^ ])\s*\)', r'for \1 in \2:', content )
+content = re.sub( r'(while|switch)\s*\(\s*(.*[^ ])\s*\)', r'\1 \2:', content )
+content = re.sub( r'class ([A-Za-z_0-9]+)', r'class \1:', content )
+content = re.sub( r'/\*\*', r'#.', content )
+content = re.sub( r'/\+\+', r'#.', content )
+content = re.sub( r'///', r'#.', content )
+content = re.sub( r'/\*', r'#', content )
+content = re.sub( r'/\+', r'#', content )
+content = re.sub( r'//', r'#', content )
+content = re.sub( r'\+\+', r' += 1', content )
+content = re.sub( r'--', r' -= 1', content )
+content = re.sub( r'if\s*\(\s*(.*[^ ])\s*\)', r'if \1:', content )
+content = re.sub( r'&&', r'and', content )
+content = re.sub( r'\|\|', r'or', content )
+content = re.sub( r'<=', r'not more than', content )
+content = re.sub( r'>=', r'not less than', content )
+content = re.sub( r'<', r'less than', content )
+content = re.sub( r' >', r' more than', content )
+content = re.sub( r'==', r'equal to', content )
+content = re.sub( r'!=', r'not equal to', content )
+content = re.sub( r'[{}]', r'', content )
+content = re.sub( r';\n', r'\n', content )
+
+with open( sys.argv[1] + "elight", "w" ) as out:
+	out.write( content )
