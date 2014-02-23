9e9c9c5dc238ebaf9e7548d71d17f2d5915c16c3
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-02-23 15:56:29 +0800

    add conversions to test

--- d2delight.sh
+++ d2delight.sh
@@ -3,9 +3,11 @@
 end="elight"
 sed -e 's/\(import.*\);/\1/g' \
 	-e 's/void \([a-zA-Z0-9_]*(.*)\)/procedure \1:/' \
+	-e 's/\(int\|string\|double\) \([a-zA-Z0-9_]\+(.*\))/function \2-> \1 ):/' \
 	-e 's/^\(\s*\)for\s*(\s*\(.*\);\s*\(.*\);\s*\(.*[^ ]\)\s*)/\1\2\n\1while \3:\n\1\t\4/' \
 	-e 's/foreach\s*(\s*\(.*\);\s*\(.*[^ ]\)\s*)/for \1 in \2:/' \
 	-e 's/\(while\|switch\)\s*(\s*\(.*[^ ]\)\s*)/\1 \2:/' \
+	-e 's/class \([A-Za-z_0-9]\+\)$/class \1:/' \
 	-e 's_/\*\*_#._' \
 	-e 's_/++_#._' \
 	-e 's_///_#._' \
@@ -20,7 +22,7 @@ sed -e 's/\(import.*\);/\1/g' \
 	-e 's/<=/not more than/g' \
 	-e 's/>=/not less than/g' \
 	-e 's/</less than/g' \
-	-e 's/>/more than/g' \
+	-e 's/ >/ more than/g' \
 	-e 's/==/equal to/g' \
 	-e 's/!=/not equal to/g' \
 	-e 's/[{}]//g' \
--- tests/d2delight.d
+++ tests/d2delight.d
@@ -1,4 +1,26 @@
+/**
+ * Block comment
+ */
 import std.stdio;
+
+int add( int a, int b )
+{
+	return a + b;
+}
+
+void print( string toprint )
+{
+	writeln( toprint );
+}
+
+class c
+{
+	int a = 5;
+	int add( int b )
+	{
+		a += b;
+	}
+}
 
 void main()
 {
@@ -7,13 +29,33 @@ void main()
 	for ( int i = 3; i < 5; i++ )
 	{
 		abcd += i;
-		if ( i < 4 )
-			abcd = 2 * 5;
+		if ( i < 4 && i > 0 )
+			abcd = add( i, 2 );
 	}
 
 	int[] array = [2,3,4,5,6];
 	foreach ( i; array )
 	{
 		abcd -= i;
+	}
+
+	sum = 1;
+	while ( sum <= 20 )
+	{
+		sum++;
+		switch ( sum )
+		{
+			case 1:
+				sum++;
+				break;
+			case 2:
+				sum += 2;
+				break;
+			case 5:
+				sum += 5;
+				break;
+			default:
+				sum = sum * 2;
+		}
 	}
 }
