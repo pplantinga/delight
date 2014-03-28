f9af1f54844ed7e8b54f2d5384b206fc3affc82a
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2014-03-28 13:46:43 +0800

    Make sure loops test finishes execution

--- tests/loops.d
+++ tests/loops.d
@@ -5,6 +5,7 @@ void main()
 {
 	int[] arr = [1, 2, 3];
 	int y = 0;
+
 	foreach (i, x; arr)
 	{
 		y += x;
@@ -12,8 +13,8 @@ void main()
 
 	foreach (j; 0 .. 5)
 	{
-		y -= j;
-		if (y < 0)
+		y *= j;
+		if (y > 0)
 		{
 			break;
 		}
@@ -21,7 +22,7 @@ void main()
 
 	foreach (k; iota(0, 10, 2))
 	{
-		y *= k;
+		y -= k;
 	}
 
 	foreach (l; map!(x=>2 * x)(iota(0, 10)).filter!(x=>x ^^ 2 > 3))
@@ -29,10 +30,10 @@ void main()
 		y /= l;
 	}
 
-	while (y < 10)
+	while (y >= -10)
 	{
-		y *= 2;
+		y %= 2;
 
-		y -= 2;
+		y -= 100;
 	}
 }
